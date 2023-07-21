ca1449c3bcddffc0c2f10844523eb86e17bf79a2
haidaralimasu <haidaralimasu123@gmail.com>
2023-07-21 19:19:54 +0530

    header section

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as HeroSection } from "./HeroSection";
 export { default as Faq } from "./Faq";
 export { default as Stats } from "./Stats";
+export { default as Testimonial } from "./Testimonial";
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Faq, HeroSection, Stats } from "../../components";
+import { Faq, HeroSection, Stats, Testimonial } from "../../components";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <Stats />
+      <Testimonial />
       <Faq />
     </div>
   );
